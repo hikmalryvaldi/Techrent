b914e4b5e290b904d9cc4342aa453e0dc1461e92
Novan <novanramdan7@gmail.com>
2024-12-01 10:02:20 +0700

    Revisi Navbar Main

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Cabin', 'sans-serif'],
+                sans: ['Roboto', 'sans-serif'],
             },
         },
     },
