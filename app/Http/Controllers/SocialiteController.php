e2eac319b8f472b9e67cddb0cabf51aa56d333ce
hikmalryvaldi <hikmalryvaldi@gmail.com>
2024-12-11 13:18:41 +0700

    Fix logout button

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -10,7 +10,8 @@ use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController extends Controller
 {
-    public function providerLogin($provider){
+    public function providerLogin($provider)
+    {
         return Socialite::driver($provider)->redirect();
     }
     // public function FacebookLogin(){
@@ -18,44 +19,44 @@ class SocialiteController extends Controller
     // }
 
     public function providerAuthentication($provider)
-{
-    // Mengambil data user dari provider (misalnya, Google)
-    $user = Socialite::driver($provider)->user();
-    
-    // Cek apakah email sudah terdaftar di database
-    $existingUser = User::where('email', $user->email)->first();
-    
-    if ($existingUser) {
-        // Jika email sudah terdaftar dan provider_id terisi (not null)
-        if($existingUser->provider_id === $user->id){
+    {
+        // Mengambil data user dari provider (misalnya, Google)
+        $user = Socialite::driver($provider)->user();
+
+        // Cek apakah email sudah terdaftar di database
+        $existingUser = User::where('email', $user->email)->first();
+
+        if ($existingUser) {
+            // Jika email sudah terdaftar dan provider_id terisi (not null)
+            if ($existingUser->id_provider === $user->id) {
+                Auth::login($existingUser);
+                return redirect()->intended('/');
+            } else {
+                // Mengarahkan ke rute /register dengan session emailTerdaftar
+                return to_route('auth')->with('emailTerdaftar', 'Email sudah terdaftar dengan metode login lain');
+            }
+
+            // Jika email terdaftar tapi belum menggunakan metode login lain (misalnya, belum ada provider_id)
+            // Proses login biasa, bisa diarahkan ke halaman dashboard atau lainnya
             Auth::login($existingUser);
             return redirect()->intended('/');
-        } else {
-        // Mengarahkan ke rute /register dengan session emailTerdaftar
-        return to_route('auth')->with('emailTerdaftar', 'Email sudah terdaftar dengan metode login lain');
         }
 
-        // Jika email terdaftar tapi belum menggunakan metode login lain (misalnya, belum ada provider_id)
-        // Proses login biasa, bisa diarahkan ke halaman dashboard atau lainnya
-        Auth::login($existingUser);
+        // Jika email belum terdaftar, buat user baru
+        $newUser = User::create([
+            'nama' => $user->name,
+            'email' => $user->email,
+            // 'phone' => '0987654321',
+            'id_provider' => $user->getId(),
+            'password' => bcrypt(Str::random(10)),
+            // kolom lain yang diperlukan seperti 'password' jika dibutuhkan
+        ]);
+
+        // Login otomatis setelah registrasi
+        auth::login($newUser);
+
         return redirect()->intended('/');
     }
-    
-    // Jika email belum terdaftar, buat user baru
-    $newUser = User::create([
-        'nama' => $user->name,
-        'email' => $user->email,
-        // 'phone' => '0987654321',
-        'id_provider' => $user->getId(),
-        'password' => bcrypt(Str::random(10)),
-        // kolom lain yang diperlukan seperti 'password' jika dibutuhkan
-    ]);
-    
-    // Login otomatis setelah registrasi
-    auth::login($newUser);
-    
-    return redirect()->intended('/');
-}
 
     // public function facebookAuthentication($provider){
     //     $User = Socialite::driver($provider)->user();
